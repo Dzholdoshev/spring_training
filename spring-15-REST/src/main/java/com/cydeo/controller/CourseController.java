f7d19105c0a202b9acfea62b8c0fc4d0441a96e1
marklen <marklen86@gmail.com>
2022-11-27 19:58:34 -0600

    Controllers added

---
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -0,0 +1,51 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@RestController
+//@RequestMapping("/courses/api/v1")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+
+    @GetMapping
+    public List<CourseDTO> getAllCourses(){
+        return courseService.getCourses();
+    }
+
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable("id") long courseId){
+        return courseService.getCourseById(courseId);
+    }
+
+    @GetMapping("category/{name}")
+    public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category){
+        return courseService.getCoursesByCategory(category);
+    }
+
+
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course){
+        return courseService.createCourse(course);
+    }
+
+    @PostMapping("{id}")
+    public void updateCourse(@PathVariable("id") Long courseId, @RequestBody CourseDTO course){
+        courseService.updateCourse(courseId,course);
+    }
+    @PostMapping("{id}")
+    public void deleteCourseById(@PathVariable("id") Long courseId){
+        courseService.deleteCourseById(courseId);
+    }
+
+
+}
