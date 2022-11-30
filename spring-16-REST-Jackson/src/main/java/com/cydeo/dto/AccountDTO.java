f7d07dc241f0c20cb308f389154070049d879acf
marklen <marklen86@gmail.com>
2022-11-29 21:09:44 -0600

    Rest Jackson annotations practice

--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
@@ -1,10 +1,15 @@
 package com.cydeo.dto;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@JsonIgnoreProperties(value = {"address","country", "state"},ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AccountDTO {
 
     private String name;
@@ -14,6 +19,9 @@ public class AccountDTO {
     private String city;
     private Integer age;
     private String postalCode;
+
+    @JsonBackReference // This field is not going to be serialized -- we don't see this object to be converted as Json
+   // @JsonIgnore // it'll ignore in get and put request
     private UserDTO user;
 
 }
